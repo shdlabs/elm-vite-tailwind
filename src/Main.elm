e44a9e3e94f4cb14c8bbce60173c93bb4ae999f1
Alex Shadrin <alexshd@gmail.com>
2023-11-26 11:21:27 +0200

    refactoring

--- src/Main.elm
+++ src/Main.elm
@@ -66,49 +66,39 @@ subscriptions _ =
 
 view : Model -> Browser.Document Msg
 view _ =
+    let
+        titleStyle =
+            { css = """
+            font-robotoV text-4xl text-transparent relative bg-gradient-to-tr z-10 border rounded-md
+            from-red-700 to-teal-700 bg-clip-text [--wght:_900] px-4 drop-shadow
+            transition-all duration-500 ease-[cubic-bezier(0.680,-0.550,0.265,1.550)] [transform-style:preserve-3d]
+            [text-shadow:3px_3px_3px_theme(colors.slate.100/.1),6px_-3px_6px_theme(colors.slate.500/.2)] text-center
+            group hover:text-slate-700 hover:[--wdth:_200] hover:[--opsz:_20] hover:[--YTLC:_614] hover:[--YTUC:_812] 
+            hover:[text-shadow:7px_9px_10px_theme(colors.teal.200/.6),-7px_-9px_10px_theme(colors.sky.300/.4)]
+            hover:rounded-xl hover:border-teal-400 hover:border-2
+            """ }
+    in
     { title = "Application Title"
     , body =
-        [ div [ tail { css = """flex justify-between flex-col gap-10 items-center m-10 relative z-0""" } ]
-            [ h1 [ tail { css = """
-                font-robotoV text-4xl text-transparent relative bg-gradient-to-tr z-10
-                from-red-700 to-teal-700 bg-clip-text [--wght:_900] px-4 drop-shadow
-                transition-all duration-500 ease-[cubic-bezier(0.680,-0.550,0.265,1.550)]
-                [text-shadow:3px_3px_3px_theme(colors.slate.100/.1),6px_-3px_6px_theme(colors.slate.500/.2)] text-center
-                [transform-style:preserve-3d] [transform:rotate3d(0,1,0,50deg)_scale(.9)_translateZ(3px)]
-                group hover:text-slate-700 hover:[--wdth:_200] hover:[--opsz:_20]
-                hover:[--YTLC:_614] hover:[--YTUC:_812] 
-
-hover:[text-shadow:7px_9px_10px_theme(colors.teal.200/.6),-7px_-9px_10px_theme(colors.sky.300/.4)]
-                """ } ]
-                [ text "New"
-                , br [] []
-                , text "Application"
-                , span [ tail { css = """
-                    absolute top-0 left-0  h-full scale-0 w-full bg-slate-100 opacity-0
-                    transition-all duration-500 ease-[cubic-bezier(0.680,-0.550,0.265,1.550)]
-                    group-hover:scale-100 group-hover:opacity-100 -z-10 group-hover:shadow-xl
-                    group-hover:rounded-lg
-                    """ } ] []
+        [ div [ tail { css = """flex justify-around flex-wrap gap-10 items-center m-10 relative z-0""" } ]
+            [ h1 [ tail titleStyle, tail { css = "[transform:rotate3d(0,-1,0,50deg)_scale(.9)_translateZ(3px)] origin-right" } ]
+                [ text "Applications"
+                , hoverBg
                 ]
-            , h1 [ tail { css = """
-                font-robotoV text-4xl text-transparent relative bg-gradient-to-tr z-10
-                from-red-700 to-teal-700 bg-clip-text [--wght:_900] px-4 drop-shadow
-                transition-all duration-500 ease-[cubic-bezier(0.680,-0.550,0.265,1.550)]
-                [text-shadow:3px_3px_3px_theme(colors.slate.100/.1),6px_-3px_6px_theme(colors.slate.500/.2)] text-center
-                [transform-style:preserve-3d] [transform:rotate3d(0,1,0,-50deg)_scale(.9)_translateZ(3px)]
-                group hover:text-slate-700 hover:[--wdth:_200] hover:[--opsz:_20]
-                hover:[--YTLC:_614] hover:[--YTUC:_812] 
-
-hover:[text-shadow:7px_9px_10px_theme(colors.teal.200/.6),-7px_-9px_10px_theme(colors.sky.300/.4)]
-                """ } ]
+            , h1 [ tail titleStyle, tail { css = "[transform:rotate3d(0,1,0,50deg)_scale(.9)_translateZ(3px)] origin-left" } ]
                 [ text "Settings"
-                , span [ tail { css = """
-                    absolute top-0 left-0  h-full scale-0 w-full bg-slate-100 opacity-0
-                    transition-all duration-500 ease-[cubic-bezier(0.680,-0.550,0.265,1.550)]
-                    group-hover:scale-100 group-hover:opacity-100 -z-10 group-hover:shadow-xl
-                    group-hover:rounded-lg
-                    """ } ] []
+                , hoverBg
                 ]
             ]
         ]
     }
+
+
+hoverBg : Html Msg
+hoverBg =
+    span [ tail { css = """
+      absolute top-0 left-0  h-full scale-0 w-full bg-slate-100 opacity-0
+      transition-all duration-500 ease-[cubic-bezier(0.680,-0.550,0.265,1.550)]
+      group-hover:scale-100 group-hover:opacity-100 -z-10 group-hover:shadow-xl
+      group-hover:rounded-xl
+      """ } ] []
